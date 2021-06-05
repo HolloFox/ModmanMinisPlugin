20b32d68d848ad0d8a0145b3a8577fd82dd4446e
brajm008 <ujkkju117@outlook.com>
2021-06-06 05:08:59 +0930

    Add project files.

---
+++ ModmanMinisPlugin/AssetsList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Bounce.Unmanaged;
+using UnityEngine;
+
+namespace ModmanMinis
+{
+    public partial class AssetsList : Form
+    {
+        public AssetsList()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChatManager.SendChatMessage("<size=0> ! " + LocalClient.SelectedCreatureId + " </size> Make me a ", (NGuid)LocalClient.SelectedCreatureId.Value);
+        }
+
+        public List<FileInfo> paths = new List<FileInfo>();
+
+        private void AssetsList_Load(object sender, EventArgs e)
+        {
+            var bundles = ModmanMinisPlugin.GetAssetPaths();
+            foreach (var path in bundles)
+            {
+                paths.Add(path);
+                listBox1.Items.Add(path.Name);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var index = listBox1.SelectedIndex;
+            var path = paths[index];
+            if (path.Exists)
+            {
+                var grand = Directory.GetParent(Directory.GetParent(path.FullName).FullName);
+                var icon = grand.FullName + "/icon.png";
+                Debug.Log(icon);
+                pictureBox1.ImageLocation = icon;
+            }
+        }
+    }
+}
---
+++ ModmanMinisPlugin/ModmanMinisPlugin.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using Bounce.Unmanaged;
+using LordAshes;
+using Newtonsoft.Json;
+using Steamworks;
+using UnityEngine;
+
+namespace ModmanMinis
+{
+
+    [BepInPlugin(Guid, "ImageToPlane", Version)]
+    [BepInDependency(CustomMiniPlugin.Guid)]
+    public class ModmanMinisPlugin: BaseUnityPlugin
+    {
+        // constants
+        private const string Guid = "org.hollofox.plugins.ModmanMinisPlugin";
+        private const string Version = "1.0.0.0";
+
+        // Configuration
+        private ConfigEntry<KeyboardShortcut> triggerKeyBasic { get; set; }
+
+        // Speech font name
+        private bool properInitialization = false;
+
+        /// <summary>
+        /// Awake plugin
+        /// </summary>
+        void Awake()
+        {
+            Debug.Log("HolloFoxes Modman Minis Plugin Active.");
+
+            var bundles = GetAssetPaths();
+            if (bundles.Count() > 0) Debug.Log("Plugin Found Minis:");
+            foreach (var path in bundles)
+            {
+                Debug.Log($"- {path.FullName}");
+            }
+
+        }
+
+        private bool isBoardLoaded()
+        {
+            return (BoardSessionManager.HasInstance &&
+                    BoardSessionManager.HasBoardAndIsInNominalState &&
+                    !BoardSessionManager.IsLoading);
+        }
+
+        /// <summary>
+        /// Looping method run by plugin
+        /// </summary>
+        void Update()
+        {
+            if (isBoardLoaded())
+            {
+                if (properInitialization && Extensions.StrictKeyCheck(triggerKeyBasic.Value))
+                {
+                    
+                }
+            }
+        }
+
+        public static ParallelQuery<FileInfo> GetAssetPaths()
+        {
+            var assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var pluginsFolder = Directory.GetParent(assemblyFolder).FullName;
+            Debug.Log($"Assembly: {assemblyFolder}");
+            Debug.Log($"Plugins: {pluginsFolder}");
+
+            DirectoryInfo dInfo = new DirectoryInfo(pluginsFolder);
+            var subdirs = dInfo.GetDirectories()
+                .AsParallel()
+                .GetSubfolders("TaleSpire_CustomData")
+                .GetSubfolders("Minis")
+                .EnumerateFiles();
+            return subdirs;
+        }
+    }
+}
